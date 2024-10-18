7d2b482531e2db5328c822399b4f470eb599ca99
Saurabh878-op <ashgamming806@gmail.com>
2024-10-18 09:38:29 +0530

    update

--- main.js
+++ main.js
@@ -1,64 +1,36 @@
-const resetBtn = document.querySelector('#reset');
-const playBtn = document.querySelector('#play');
-const timerEl = document.querySelector('#timer');
-const root = document.querySelector(':root');
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
 
-// Initial setup
-const totalSeconds = 60;
-let playing = false;
-let currentSeconds = totalSeconds;
-timerEl.innerText = formatTime(totalSeconds);
+jokeBtn.addEventListener('click', generateJoke)
 
-const timerInterval = setInterval(run, 1000);
+generateJoke()
 
-playBtn.addEventListener('click', () => {
-  playing = !playing;
-  playBtn.classList.toggle('play');
-  playBtn.classList.toggle('bg-green-500'); // Toggle the color class
-  const playIcon = playBtn.querySelector('i');
-  playIcon.classList.toggle('fa-play'); // Toggle the play icon
-  playIcon.classList.toggle('fa-pause'); // Toggle the pause icon
-});
-resetBtn.addEventListener('click', resetAll);
-
-// Run the timer
-function run() {
-  if (playing) {
-    currentSeconds -= 1;
-    if (currentSeconds <= 0) {
-      clearInterval(timerInterval);
-      resetAll();
-    }
-
-    timerEl.innerText = formatTime(currentSeconds);
-    root.style.setProperty('--degrees', calcDeg());
+// USING ASYNC/AWAIT
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
   }
+
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  jokeEl.innerHTML = data.joke
 }
 
-// Format the time
-function formatTime(seconds) {
-  const minutes = Math.floor(seconds / 60);
-  const newSeconds = seconds % 60;
+// USING .then()
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   }
 
-  return `${minutes.toString().padStart(2, '0')}:${newSeconds
-    .toString()
-    .padStart(2, '0')}`;
-}
-
-// Calculate the degrees
-function calcDeg() {
-  return `${360 - (currentSeconds / totalSeconds) * 360}deg`;
-}
-
-// Reset all the values
-function resetAll() {
-  playing = false;
-  playBtn.classList.remove('play');
-  playBtn.classList.remove('bg-green-500'); // Remove the color class
-  const playIcon = playBtn.querySelector('i');
-  playIcon.classList.remove('fa-pause'); // Remove the pause icon
-  playIcon.classList.add('fa-play'); // Add the play icon
-  currentSeconds = totalSeconds;
-  timerEl.innerText = formatTime(totalSeconds);
-  root.style.setProperty('--degrees', '0deg');
-}
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke
+//     })
+// }
