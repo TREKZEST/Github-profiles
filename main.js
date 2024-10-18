d8d5ed8421ebb9003ea706f6e838c0d82916c6a3
Saurabh878-op <ashgamming806@gmail.com>
2024-10-18 11:03:24 +0530

    update

--- main.js
+++ main.js
@@ -1,69 +1,17 @@
-const tagsEl = document.getElementById('tags')
-const textarea = document.getElementById('textarea')
+const container = document.querySelector('.container')
+const unsplashURL = 'https://source.unsplash.com/random/'
+const rows = 5
 
-textarea.focus()
-
-textarea.addEventListener('keyup', (e) => {
-    createTags(e.target.value)
-
-    if(e.key === 'Enter') {
-        setTimeout(() => {
-            e.target.value = ''
-        }, 10)
-
-        randomSelect()
-    }
-})
-
-function createTags(input) {
-    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
-
-    tagsEl.innerHTML = ''
-
-    tags.forEach(tag => {
-        const tagEl = document.createElement('span')
-        tagEl.classList.add('tag')
-        tagEl.innerText = tag
-        tagsEl.appendChild(tagEl)
-    })
+for(let i = 0; i < rows * 3; i++) {
+    const img = document.createElement('img')
+    img.src = `${unsplashURL}${getRandomSize()}`
+    container.appendChild(img)
 }
 
-function randomSelect() {
-    const times = 30
-
-    const interval = setInterval(() => {
-        const randomTag = pickRandomTag()
-
-        if (randomTag !== undefined) {
-        highlightTag(randomTag)
-
-        setTimeout(() => {
-            unHighlightTag(randomTag)
-        }, 100)
-        }
-    }, 100);
-
-    setTimeout(() => {
-        clearInterval(interval)
-
-        setTimeout(() => {
-            const randomTag = pickRandomTag()
-
-            highlightTag(randomTag)
-        }, 100)
-
-    }, times * 100)
+function getRandomSize() {
+    return `${getRandomNr()}x${getRandomNr()}`
 }
 
-function pickRandomTag() {
-    const tags = document.querySelectorAll('.tag')
-    return tags[Math.floor(Math.random() * tags.length)]
-}
-
-function highlightTag(tag) {
-    tag.classList.add('highlight')
-}
-
-function unHighlightTag(tag) {
-    tag.classList.remove('highlight')
+function getRandomNr() {
+    return Math.floor(Math.random() * 10) + 300
 }
