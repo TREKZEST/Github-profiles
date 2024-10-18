c6a255564aa1250f19ded8593a5cba729f629d52
Saurabh878-op <ashgamming806@gmail.com>
2024-10-18 16:07:24 +0530

    update

--- main.js
+++ main.js
@@ -1,40 +1,91 @@
-const nums = document.querySelectorAll('.nums span')
-const counter = document.querySelector('.counter')
-const finalMessage = document.querySelector('.final')
-const replay = document.querySelector('#replay')
+const APIURL = 'https://api.github.com/users/'
 
-runAnimation()
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
 
-function resetDOM() {
-  counter.classList.remove('hide')
-  finalMessage.classList.remove('show')
+async function getUser(username) {
+    try {
+        const { data } = await axios(APIURL + username)
 
-  nums.forEach((num) => {
-    num.classList.value = ''
-  })
-
-  nums[0].classList.add('in')
+        createUserCard(data)
+        getRepos(username)
+    } catch(err) {
+        if(err.response.status == 404) {
+            createErrorCard('No profile with this username')
+        }
+    }
 }
 
-function runAnimation() {
-  nums.forEach((num, idx) => {
-    const nextToLast = nums.length - 1
+async function getRepos(username) {
+    try {
+        const { data } = await axios(APIURL + username + '/repos?sort=created')
 
-    num.addEventListener('animationend', (e) => {
-      if (e.animationName === 'goIn' && idx !== nextToLast) {
-        num.classList.remove('in')
-        num.classList.add('out')
-      } else if (e.animationName === 'goOut' && num.nextElementSibling) {
-        num.nextElementSibling.classList.add('in')
-      } else {
-        counter.classList.add('hide')
-        finalMessage.classList.add('show')
-      }
-    })
-  })
+        addReposToCard(data)
+    } catch(err) {
+        createErrorCard('Problem fetching repos')
+    }
 }
 
-replay.addEventListener('click', () => {
-  resetDOM()
-  runAnimation()
+function createUserCard(user) {
+    const userID = user.name || user.login
+    const userBio = user.bio ? `<p>${user.bio}</p>` : ''
+    const cardHTML = `
+    <div class="card">
+    <div>
+      <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+    <div class="user-info">
+      <h2>${userID}</h2>
+      ${userBio}
+      <ul>
+        <li>${user.followers} <strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li>${user.public_repos} <strong>Repos</strong></li>
+      </ul>
+
+      <div id="repos"></div>
+    </div>
+  </div>
+    `
+    main.innerHTML = cardHTML
+
+}
+
+function createErrorCard(msg) {
+    const cardHTML = `
+        <div class="card">
+            <h1>${msg}</h1>
+        </div>
+    `
+
+    main.innerHTML = cardHTML
+}
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos')
+
+    repos
+        .slice(0, 5)
+        .forEach(repo => {
+            const repoEl = document.createElement('a')
+            repoEl.classList.add('repo')
+            repoEl.href = repo.html_url
+            repoEl.target = '_blank'
+            repoEl.innerText = repo.name
+
+            reposEl.appendChild(repoEl)
+        })
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value
+
+    if(user) {
+        getUser(user)
+
+        search.value = ''
+    }
 })
