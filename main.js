16eb3fd12077bc99748c4480206bc97586b6cf99
Saurabh878-op <ashgamming806@gmail.com>
2024-10-18 15:49:17 +0530

    update

--- main.js
+++ main.js
@@ -1,98 +1,49 @@
-const quizData = [
-    {
-        question: "Which language runs in a web browser?",
-        a: "Java",
-        b: "C",
-        c: "Python",
-        d: "JavaScript",
-        correct: "d",
-    },
-    {
-        question: "What does CSS stand for?",
-        a: "Central Style Sheets",
-        b: "Cascading Style Sheets",
-        c: "Cascading Simple Sheets",
-        d: "Cars SUVs Sailboats",
-        correct: "b",
-    },
-    {
-        question: "What does HTML stand for?",
-        a: "Hypertext Markup Language",
-        b: "Hypertext Markdown Language",
-        c: "Hyperloop Machine Language",
-        d: "Helicopters Terminals Motorboats Lamborginis",
-        correct: "a",
-    },
-    {
-        question: "What year was JavaScript launched?",
-        a: "1996",
-        b: "1995",
-        c: "1994",
-        d: "none of the above",
-        correct: "b",
-    },
-];
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
 
-const quiz = document.getElementById('quiz')
-const answerEls = document.querySelectorAll('.answer')
-const questionEl = document.getElementById('question')
-const a_text = document.getElementById('a_text')
-const b_text = document.getElementById('b_text')
-const c_text = document.getElementById('c_text')
-const d_text = document.getElementById('d_text')
-const submitBtn = document.getElementById('submit')
+let currentActive = 1
 
-let currentQuiz = 0
-let score = 0
+next.addEventListener('click', () => {
+    currentActive++
 
-loadQuiz()
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
 
-function loadQuiz() {
-    deselectAnswers()
+    update()
+})
 
-    const currentQuizData = quizData[currentQuiz]
+prev.addEventListener('click', () => {
+    currentActive--
 
-    questionEl.innerText = currentQuizData.question
-    a_text.innerText = currentQuizData.a
-    b_text.innerText = currentQuizData.b
-    c_text.innerText = currentQuizData.c
-    d_text.innerText = currentQuizData.d
-}
+    if(currentActive < 1) {
+        currentActive = 1
+    }
 
-function deselectAnswers() {
-    answerEls.forEach(answerEl => answerEl.checked = false)
-}
+    update()
+})
 
-function getSelected() {
-    let answer
-
-    answerEls.forEach(answerEl => {
-        if(answerEl.checked) {
-            answer = answerEl.id
+function update() {
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
         }
     })
 
-    return answer
-}
+    const actives = document.querySelectorAll('.active')
 
-submitBtn.addEventListener('click', () => {
-    const answer = getSelected()
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
 
-    if(answer) {
-        if(answer === quizData[currentQuiz].correct) {
-            score++
-        }
-
-        currentQuiz++
-
-        if(currentQuiz < quizData.length) {
-            loadQuiz()
-        } else {
-            quiz.innerHTML = `
-                <h2>You answered ${score}/${quizData.length} questions correctly</h2>
-
-                <button onclick="location.reload()">Reload</button>
-            `
-        }
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if(currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
     }
-})
+}
